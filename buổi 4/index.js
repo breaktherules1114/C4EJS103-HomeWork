bd8420cadf57589e2e81dd8023978b9f7f1b09e1
breaktherules1114 <74419132+breaktherules1114@users.noreply.github.com>
2020-11-27 16:56:47 +0700

    b4

--- buổi 4/index.js
+++ buổi 4/index.js
@@ -35,3 +35,47 @@ let last = function(arr, n) {
 console.log(last([7, 9, 0, -2]))
 console.log(last([7, 9, 0, -2],3))
 console.log(last([7, 9, 0, -2],6))
+
+myColor = ["Red", "Green", "White", "Black"]
+console.log(myColor.toString())
+console.log(myColor.join())
+console.log(myColor.join('+'))
+
+let num=window.prompt()
+let str = num.toString()
+let result = [str[0]]
+  
+for(let x=1; x<str.length; x++)
+  {
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+     {
+      result.push('-', str[x])
+     }
+    else
+     {
+      result.push(str[x])
+     }
+  }
+console.log(result.join(''))
+
+let chuoi = 'Chac Ai do Se Ve'
+let UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let LOWER = 'abcdefghijklmnopqrstuvwxyz'
+let result1 = ""
+  
+  for(let x=0; x<chuoi.length; x++)
+  {
+    if(UPPER.indexOf(chuoi[x]) !== -1)
+    {
+      result1=result1+chuoi[x].toLowerCase()
+    }
+    else if(LOWER.indexOf(chuoi[x]) !== -1)
+    {
+      result1=result1+chuoi[x].toUpperCase()
+    }
+    else 
+    {
+      result1=result1+chuoi[x]
+    }
+  }
+console.log(result1)
